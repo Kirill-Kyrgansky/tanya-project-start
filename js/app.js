1bc80e130c8e594f0595aa7fc728cade1cc89702
Kirill-Kyrgansky <kirill-Kyrgansky@gmail.com>
2025-12-17 20:50:13 +0300

    added animation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
-function test1() {
+function submit() {
+  const button = document.querySelector('.button');
+  const input = document.querySelector('#input');
+  const form = document.getElementById('form');
+  const secondAnimate = document.getElementById('secondWrap');
+  button.addEventListener('click', () => {
+
+    const email = input.value.trim();
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+    if (isValidEmail) {
+      form.style.display = 'none';
+      requestAnimationFrame(() => {
+        secondAnimate.innerHTML = `
+          <img src="/img/second.svg" id="second" alt=""
+               style="width: 100%; height: 100vh;">
+        `;
+      });
+    }
+  })
+}
+
+
+function startAnimation() {
   const firstAnimate = document.getElementById('first');
   const form = document.getElementById('form');
   form.style.display = 'none';
@@ -8,4 +31,5 @@ function test1() {
   }, 4000)
 }
 
-test1()
+startAnimation()
+submit()
